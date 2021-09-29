c2db5744dc929b7af8f48760539a7e0723736f36
Seth Wiley <seth.wiley@temple.edu>
2021-09-29 12:30:16 -0400

    Initial Commit in new project cause old project broke

---
+++ app/src/main/java/com/adelmotechnology/recyclergrid/ImageAdapter.kt
@@ -0,0 +1,49 @@
+package com.adelmotechnology.recyclergrid
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ImageAdapter(var context: Context) : RecyclerView.Adapter<ImageAdapter.ViewHolder>() {
+
+    var dataList = emptyList<DataModel>()
+
+    internal fun setDataList(dataList: List<DataModel>) {
+        this.dataList = dataList
+    }
+
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var image: ImageView
+        var title: TextView
+        var desc: TextView
+
+        init {
+            image = itemView.findViewById(R.id.image)
+            title = itemView.findViewById(R.id.title)
+            desc = itemView.findViewById(R.id.desc)
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageAdapter.ViewHolder {
+        var view = LayoutInflater.from(parent.context).inflate(R.layout.photo_layout, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ImageAdapter.ViewHolder, position: Int) {
+        var data = dataList[position]
+
+        holder.title.text = data.title
+        holder.desc.text = data.desc
+
+        holder.image.setImageResource(data.image)
+    }
+
+
+    override fun getItemCount() = dataList.size
+}
---
+++ app/src/main/java/com/adelmotechnology/recyclergrid/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.adelmotechnology.recyclergrid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var  imageAdapter: ImageAdapter
+    private var dataList = mutableListOf<DataModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+        recyclerView.layoutManager = GridLayoutManager(applicationContext,3)
+        imageAdapter = ImageAdapter(applicationContext)
+        recyclerView.adapter = imageAdapter
+
+        dataList.add(DataModel("1st Cheese","Some Cheese",R.drawable.cheese1))
+        dataList.add(DataModel("2nd Cheese","Different Cheese",R.drawable.cheese2))
+        dataList.add(DataModel("3rd Cheese","Other Cheese",R.drawable.cheese3))
+        dataList.add(DataModel("4th Cheese","New Cheese",R.drawable.cheese4))
+        dataList.add(DataModel("5th Cheese","Fun Cheese",R.drawable.cheese5))
+        dataList.add(DataModel("6th Cheese","Weird Cheese",R.drawable.cheese6))
+        dataList.add(DataModel("7th Cheese","Another Cheese",R.drawable.cheese7))
+        dataList.add(DataModel("8th Cheese","WOW, cheese!",R.drawable.cheese8))
+        dataList.add(DataModel("9th Cheese","old cheese",R.drawable.cheese9))
+        dataList.add(DataModel("10th Cheese","woah, cheese",R.drawable.cheese10))
+        dataList.add(DataModel("11th Cheese","this is cheese",R.drawable.cheese11))
+        dataList.add(DataModel("12th Cheese","more cheese",R.drawable.cheese12))
+
+
+        imageAdapter.setDataList(dataList)
+
+
+
+
+    }
+}
